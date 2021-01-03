4b8edc9a5b43af630007bbb8e2fb44830cb9bfba
ZoraizQ <21100130@lums.edu.pk>
2021-01-03 05:49:32 +0500

    add: loading backdrop and error snackbar

--- client/src/components/Main/MainScreen.js
+++ client/src/components/Main/MainScreen.js
@@ -8,12 +8,30 @@ import MessageBox from './Items/MessageBox/MessageBox'
 import io from "socket.io-client"
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import DescriptionIcon from '@material-ui/icons/Description'
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
 
 let socket;
 let user;
 
-export default function MainScreen(){
+export default function MainScreen() {
+    const classes = useStyles();
+    let [snackbarMsg, setSnackbarMsg] = useState('');
     let [chats, setChats] = useState({});
     let [connected, setConnected] = useState(false);
     let [session, setSession] = useState({});
@@ -61,7 +79,7 @@ export default function MainScreen(){
         // Error Handler
         socket.on('err', (err) => {
             console.error('err:', err);
-            alert(JSON.stringify(err));
+            setSnackbarMsg(`${err.name}: ${err.details}`);
         });
 
         /**
@@ -1052,6 +1070,11 @@ export default function MainScreen(){
         return req;
     }
 
+    function handleSnackbarClose() {
+        setSnackbarMsg('');
+    }
+
+
     return(
         <div className='parent'>
             <div className= 'Information-Bar'><InformationBar/>
@@ -1103,7 +1126,14 @@ export default function MainScreen(){
                     </div>
                 </div>
             }
-            
+            <Backdrop className={classes.backdrop} open={!connected}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <Snackbar open={snackbarMsg !== ''} onClose={handleSnackbarClose} anchorOrigin={{vertical: 'top', horizontal: 'center'}} autoHideDuration={6000}>
+                <Alert onClose={handleSnackbarClose} severity="error">
+                    {snackbarMsg}
+                </Alert>
+            </Snackbar>
         </div>
         
     )
