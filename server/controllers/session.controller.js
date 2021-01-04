506bcc524f0843f4e2c388b2067f4fcec020b79f
drageelr <56049229+drageelr@users.noreply.github.com>
2021-01-04 12:58:57 +0500

    join api created

--- server/controllers/session.controller.js
+++ server/controllers/session.controller.js
@@ -63,3 +63,31 @@ exports.stopSession = async (req, res, next) => {
         next(err);
     }
 }
+
+exports.joinSession = async (req, res, next) => {
+    try {
+        let params = req.body;
+
+        if (params.user.type != 'admin' && params.user.type != 'dias') { throw new customError.ForbiddenAccessError("you are not allowed to access this api"); }
+
+        let committeeId = params.committeeId;
+        if (params.user.type == 'dias') {
+            let reqDias = await db.query('SELECT committeeId FROM dias WHERE id = ' + params.user.id);
+            committeeId = reqDias[0].committeeId;
+        }
+
+        let reqSession = await db.query('SELECT id FROM session WHERE active = 1 AND committeeId = ' + committeeId);
+
+        if (!reqSession.length) { throw new customError.NotFoundError("committe session is not in progress"); }
+
+        res.json({
+            statusCode: 200,
+            statusName: httpStatus.getName(200),
+            message: "Session " + reqSession[0].id + " is in progress!",
+            sessionId: reqSession[0].id
+        });
+
+    } catch (err) {
+        next(err);
+    }
+}
